6032064c58eacae265efa9f2cb5198e4eb6d3cd1
“LaurentDubosq” <laurent.dubosq@gmail.com>
2024-04-15 11:15:00 +0200

    feature: creation of website header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <header></header>
+  <Header />
   <RouterView />
 </template>
 
