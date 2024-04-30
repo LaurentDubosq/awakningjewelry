3e3b9f25d6e9f8c32297ca9e7bc48b6eba154cb9
“LaurentDubosq” <laurent.dubosq@gmail.com>
2024-04-30 15:51:05 +0200

    feature(transition): creation of reusable transition component and addition of translateY transition

---
+++ src/components/MyTransition.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { name } = defineProps({ name: { type: String, required: true } });
+</script>
+
+<template>
+  <Transition :name="name">
+    <slot />
+  </Transition>
+</template>
+
+<style lang="scss">
+.translateY-enter-active {
+  transition: all 0.3s ease;
+}
+
+.translateY-leave-active {
+  transition: all;
+}
+
+.translateY-enter-from,
+.translateY-leave-to {
+  transform: translateY(-15px);
+}
+</style>
