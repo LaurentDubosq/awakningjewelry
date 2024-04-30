f462d5fffd7f35cb76deea2e77b44cfe0f6cc665
“LaurentDubosq” <laurent.dubosq@gmail.com>
2024-04-30 15:55:38 +0200

    feature(siteLogo): creation of SiteLogo component

---
+++ src/components/SiteLogo.vue
@@ -0,0 +1,8 @@
+<template>
+  <img
+    src="@/assets/logo.svg"
+    alt="AwakningJewelry's logo - Illustration of a person medidating above the AwakningJewelry brand name"
+    title="Innovante Buddhist Jewelry Brand - AwakningJewelry.com"
+    class="site-header__logo site-header__logo--size-desktop"
+  />
+</template>
