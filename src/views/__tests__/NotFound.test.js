d712ff9da808ef3afea15f2d175bb1977c32ed82
LaurentDubosq <laurent.dubosq@gmail.com>
2025-01-24 14:28:57 +0100

    feat(NotFound): addition of the NotFound.vue's tests

---
+++ src/views/__tests__/NotFound.test.js
@@ -0,0 +1,40 @@
+import { mount, RouterLinkStub } from "@vue/test-utils";
+import NotFound from "../NotFound.vue";
+
+// Component Factory
+function mountNotFound() {
+  return mount(NotFound, {
+    global: {
+      stubs: {
+        RouterLink: RouterLinkStub,
+      },
+    },
+  });
+}
+
+describe("NotFound.vue", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mountNotFound();
+  });
+
+  // Smoke test
+  test("mounts successfully", () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  test("the component has not been improperly modified", () => {
+    expect(wrapper.html()).toMatchInlineSnapshot(`
+      "<div class="not-found-container wrapper">
+        <h1>Not Found</h1>
+        <p> Oops, we couldn't find that page. Try going <a>home</a></p>
+      </div>"
+    `);
+  });
+
+  test("its redirection link has the correct url", () => {
+    const link = wrapper.findComponent(RouterLinkStub);
+    expect(link.props("to")).toBe("/");
+  });
+});
